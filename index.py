dcb2427f4d35a7ef3702078969487f71a76e15bd
skyone <skyone.wzw@qq.com>
2021-10-24 22:24:43 +0800

    set action

--- index.py
+++ index.py
@@ -30,33 +30,36 @@ for i in range(start, end):
         index = index + 1
 
 task.reverse()
-while len(task) > 0:
-    image = task.pop()
-    url = "https://www.pixiv.net/artworks/" + image["id"]
-    try:
-        response = requests.get(url, headers=headers)
-        response.encoding = response.apparent_encoding
-        selector = html.fromstring(response.text)
-        image_info = selector.xpath(r'//*[@id="meta-preload-data"]/@content')[0]
-        image_info = json.loads(image_info)
-        user_info = image_info["user"][list(image_info["user"].keys())[0]]
-        illust_info = image_info["illust"][list(image_info["illust"].keys())[0]]
-        json_data = json.dumps({"id": image["index"], "user": user_info, "image": illust_info, "pid": image["id"]},
-                               ensure_ascii=False)
-        save_path = "output/" + "{:0>6}".format(image["index"]) + "_" + image["id"] + ".json"
-        with open(save_path, "w", encoding="utf-8") as f:
-            f.write(json_data)
-        print("[info]\t%d\t%s\t%s" % (image["index"], image["id"], illust_info["title"]))
-        time.sleep(1)
-    except Exception:
-        print("[error]\t%d\t%s\t try again" % (image["index"], image["id"]))
-        time.sleep(10)
-        image["fail"] += 1
-        if image["fail"] <= 5:
-            task.insert(image, 0)
-        else:
-            print("[error]\t can't load %d\t%s" % (image["index"], image["id"]))
-            fail.append(image)
-
-with open("dataset/fail.json", "w") as f:
-    f.write(json.dumps(fail))
+try:
+    while len(task) > 0:
+        image = task.pop()
+        url = "https://www.pixiv.net/artworks/" + image["id"]
+        try:
+            response = requests.get(url, headers=headers)
+            response.encoding = response.apparent_encoding
+            selector = html.fromstring(response.text)
+            image_info = selector.xpath(r'//*[@id="meta-preload-data"]/@content')[0]
+            image_info = json.loads(image_info)
+            user_info = image_info["user"][list(image_info["user"].keys())[0]]
+            illust_info = image_info["illust"][list(image_info["illust"].keys())[0]]
+            json_data = json.dumps({"id": image["index"], "user": user_info, "image": illust_info, "pid": image["id"]},
+                                   ensure_ascii=False)
+            save_path = "output/" + "{:0>6}".format(image["index"]) + "_" + image["id"] + ".json"
+            with open(save_path, "w", encoding="utf-8") as f:
+                f.write(json_data)
+            print("[info]\t%d\t%s\t%s" % (image["index"], image["id"], illust_info["title"]))
+            time.sleep(1.5)
+        except Exception:
+            print("[error]\t%d\t%s\t try again" % (image["index"], image["id"]))
+            time.sleep(10)
+            image["fail"] += 1
+            if image["fail"] <= 5:
+                task.insert(image, 0)
+            else:
+                print("[error]\t can't load %d\t%s" % (image["index"], image["id"]))
+                fail.append(image)
+        finally:
+            with open("dataset/fail.json", "w") as f:
+                f.write(json.dumps(fail))
+except Exception as e:
+    print(e)
